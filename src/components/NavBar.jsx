f2c78fc53fc50958e8baf9026959cdf765e8f93d
abhi-369 <abhiprajapati1827@gmail.com>
2026-06-17 17:01:36 +0530

    3d-site

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,32 +1,28 @@
-import React from 'react'
-import {navLinks} from '../constants'
+import {navLinks} from "../constants";
 
 const NavBar = () => {
     return (
         <header>
             <nav>
-                <img src="/logo.svg" alt="Apple logo"/>
+                <img  src="/logo.svg" alt="Apple logo" />
 
                 <ul>
-                    {navLinks.map(({label}) => (
-                            <li key={label}>
-                                <a href={label}>{label}</a>
-                            </li>
-                        ))
-                    }
-
+                    {navLinks.map(({ label }) => (
+                        <li key={label}>
+                            <a href={label}>{label}</a>
+                        </li>
+                    ))}
                 </ul>
 
                 <div className="flex-center gap-3">
                     <button>
-                        <img src="/search.svg" alt="Search"/>
+                        <img src="/search.svg" alt="Search" />
                     </button>
                     <button>
-                        <img src="/cart.svg" alt="Cart"/>
+                        <img src="/cart.svg" alt="Cart" />
                     </button>
                 </div>
             </nav>
-
         </header>
     )
 }
